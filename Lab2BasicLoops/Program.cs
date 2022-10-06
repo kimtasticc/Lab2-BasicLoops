272dfb407355d9d3ca6ff0e60512dd296d82a1f9
kim <kimbervro@gmail.com>
2022-10-06 16:06:53 -0400

    Add project files.

---
+++ Lab2BasicLoops/Program.cs
@@ -0,0 +1,81 @@
+﻿bool userContinue;
+
+do
+{
+    Console.WriteLine("Hello World!");
+
+
+    Console.WriteLine();
+
+
+    Console.WriteLine("Please give me a number: ");
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine($"You chose {userNumber}! Let's do some counting exercises.");
+
+    for (int i = userNumber; i >= 0; i--)
+    {
+        Console.WriteLine(i);
+    }
+
+    Console.WriteLine();
+
+    for (int i = 0; i <= userNumber; i++)
+    {
+        Console.WriteLine(i);
+    }
+
+
+    Console.WriteLine();
+
+
+    Console.WriteLine("Would you like to continue on? (y/n)?");
+    string answer = Console.ReadLine();
+
+    if (answer.ToLower() == "n")
+    {
+        userContinue = false;
+        Console.WriteLine("Okay cya!");
+    }
+    else
+    {
+        userContinue = true;
+    }
+} while (userContinue == true);
+
+
+Console.WriteLine();
+
+
+int securityCode = 13579;
+int guessAttempt = 5;
+bool doorUnlocked = false;
+
+Console.WriteLine("Please enter the correct keycode to unlock this door.");
+int userInput = Convert.ToInt32(Console.ReadLine());
+
+while (doorUnlocked == false)
+{
+    guessAttempt--;
+
+    if (userInput == securityCode)
+    {
+        Console.WriteLine("The door has been unlocked. Come on in!");
+        doorUnlocked = true;
+    }
+        
+    else if (guessAttempt > 0)
+    {
+        Console.WriteLine("Sorry. Please enter the correct keycode to unlock this door.");
+        Console.WriteLine($"You have {guessAttempt} more tries.");
+        userInput = Convert.ToInt32(Console.ReadLine());
+    }
+
+    else
+    {
+        Console.WriteLine("You have too many incorrect attempts. You might want to call someone to let you in.");
+        break;
+    }
+}
+
+Console.ReadKey();
---
+++ Lab2BasicLoopsProject2/Program.cs
@@ -0,0 +1,67 @@
+﻿int securityCode = 13579;
+int guessAttempt = 5;
+
+bool doorUnlocked = false;
+
+Console.WriteLine("Please enter the correct keycode to unlock this door.");
+int userInput = Convert.ToInt32(Console.ReadLine());
+
+do
+{
+    guessAttempt--;
+
+    if (userInput == securityCode)
+    {
+        Console.WriteLine("The door has been unlocked. Come on in!");
+        doorUnlocked = true;
+    }
+
+    else if (guessAttempt > 0)
+    {
+        Console.WriteLine("Sorry. Please enter the correct keycode to unlock this door.");
+        Console.WriteLine($"You have {guessAttempt} more tries.");
+        userInput = Convert.ToInt32(Console.ReadLine());
+    }
+
+    else
+    {
+        Console.WriteLine("You have too many incorrect attempts. You might want to call someone to let you in.");
+        break;
+    }
+} while (doorUnlocked == false);
+
+Console.ReadKey();
+
+// My attempt at the Optional Stretch is below. It seems to run as intended so please let me know if the coding is what you would expect?
+// I'm having a pretty tough time understanding methods. Thanks!
+
+DoorKeyCode(securityCode, guessAttempt);
+
+bool DoorKeyCode(int securityCode, int guessAttempt)
+{
+    Console.WriteLine("Please enter the correct keycode to unlock this door.");
+    int userInput = Convert.ToInt32(Console.ReadLine());
+
+    while (userInput != securityCode)
+    {
+        guessAttempt--;
+
+        if (guessAttempt > 0)
+        {
+            Console.WriteLine("Sorry. Please enter the correct keycode to unlock this door.");
+            Console.WriteLine($"You have {guessAttempt} more tries.");
+            userInput = Convert.ToInt32(Console.ReadLine());
+        }
+
+        else
+        {
+            Console.WriteLine("You have too many incorrect attempts. You might want to call someone to let you in.");
+            return false;
+        }
+    }
+
+    Console.WriteLine("The door has been unlocked. Come on in!");
+    return true;
+}
+
+Console.ReadKey();
